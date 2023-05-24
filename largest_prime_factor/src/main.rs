52bfa6503e48232545f18a701eaf5b40254182bb
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-05-24 16:59:49 -0300

    refactor: largest_prime_factor refactored

--- largest_prime_factor/src/main.rs
+++ largest_prime_factor/src/main.rs
@@ -5,29 +5,25 @@ fn main() {
 fn largest_prime_factor(x: i64) -> f64 {
     let mut largest: f64 = -1.0;
     let mut x_copy:f64 = x.clone() as f64;
-    let x_start_value:f64 = x.clone() as f64;
+    let upper_limit:i64 = (x as f64).sqrt() as i64;
 
     while x_copy % 2.0 == 0.0 {
         largest = 2.0;
         x_copy /= 2.0;
     }
-
     while x_copy % 3.0 == 0.0 {
         largest = 3.0;
         x_copy /= 3.0;
     }
-    let mut i: f64 = 5.0;
-    while i <= x_start_value.sqrt()
-    {
-        while x_copy % i == 0.0 {
-            largest = i;
-            x_copy = x_copy / i;
+    for i in (5..upper_limit).step_by(6) {
+        while x_copy % i as f64 == 0.0 {
+            largest = i as f64;
+            x_copy = x_copy / i as f64;
         }
-        while x_copy % (i + 2.0) == 0.0 {
-            largest = i + 2.0;
-            x_copy = x_copy / (i + 2.0);
+        while x_copy % (i + 2) as f64 == 0.0 {
+            largest = (i + 2) as f64;
+            x_copy = x_copy / (i + 2) as f64;
         }
-        i += 6.0;
     }
     largest
 }
