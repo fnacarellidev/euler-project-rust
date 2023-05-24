763fca55aca0e0ad091d8afc11b8143d540fb1ac
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-05-24 15:46:13 -0300

    feat: 2nd solution to even_fibonacci_numbers (recursively)

--- even_fibonacci_numbers/src/main.rs
+++ even_fibonacci_numbers/src/main.rs
@@ -1,22 +1,13 @@
 fn main() {
-    let mut current = 1;
-    let mut previous = 0;
-    let mut total = 0;
-    let mut aux;
-
-    loop {
-        aux = current + previous;
-        previous = current;
-        current = aux;
-        if aux % 2 == 0 { total += aux; }
-        if aux >= 4000000 { break; }
-    } 
-    println!("{}", total);
+    println!("{}", fibo(0, 1));
 }
 
-// fn fibo(x: i32, y: i32) -> i32 {
-//     if x + y >= 13 { return 0; }
-//     fibo(y, x + y);
-//     if x + y % 2 == 0 { x + y }
-//     else { return 0; }
-// }
+fn fibo(x: i32, y: i32) -> i32 {
+    if x + y >= 4000000 { return 0; }
+    let res = fibo(y, x + y);
+    if (x + y) % 2 == 0 {
+        res + x + y
+    } else {
+        return res;
+    }
+}
